2a3e6e26e394e29da6892d84be48f11a8e009150
Matei-Marius Micu <micumatei@gmail.com>
2018-06-04 13:41:42 +0300

    Add scripts locally

---
+++ content/scripts/destroy.sh
@@ -0,0 +1,9 @@
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/273ffee7d9e1cb9d4a1a45cfed037a97/raw/61956bc815cf6e257dc0dfc8c38ce940782833cf/03_empty_dir.yml
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/463c2706fe7404a18bc2343da90dbad1/raw/04561abc4597733376d16b4d429d5e50167bae85/04_empty_dir_service.yml
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/8b6ff7d06e5413862438afc2c12c5357/raw/154b19718ec5c67d828d16c642700980c211f59a/01_deployment.yal
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/1c7191ae666ca7aed7656f59e61d0937/raw/bfc14eab460bfd450150a6e2a4826016b1a7aff5/02_service.yml
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/3b6c174cfb21e87cfbf22e3eb0291ef5/raw/acdb468a148d67546e4432bb41f8787a06e875aa/05_secret.yml
+kubectl delete secret mambu-secret2
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/a9ddb794bf0cc62442d19eee3c2fd86c/raw/df7aa7a18b7a41c8ffa62c0e9754bc79414132eb/06_sercret_pod.yml
+kubectl delete -f https://gist.githubusercontent.com/mateimicu/e192589968d4876a6192712bc07e1bc5/raw/4514eedddeffc498cb0c09d1f9a3fadabf4da5b7/07_secret_service.yml
+
